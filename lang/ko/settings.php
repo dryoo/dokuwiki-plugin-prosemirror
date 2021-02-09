7dd3d3ec2d5a6ccc6ea1e3c74c3f5157b0dc6d61
SC Yoo <dryoo@live.com>
2021-02-09 16:23:07 +0900

    Create settings.php

---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * korean language file for prosemirror plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+// keys need to match the config setting name
+$lang['forceWYSIWYG'] = '모든 사용자에게 강제로 WYSIWYG 편집기 사용하도록 하기 (관리자와 매니저 제외)';
