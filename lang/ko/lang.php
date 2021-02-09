2e9c741fbaef9ecfc0b04b6a014384f9947d32b0
SC Yoo <dryoo@live.com>
2021-02-09 16:21:29 +0900

    Create lang.php

---
+++ lang/ko/lang.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Korean language file for prosemirror plugin
+ * lang.php
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+// custom language strings for the plugin
+$lang['switch_editors'] = 'WYSIWYG 편집기 전환';
+
+$lang['link target'] = '대상 링크';
+$lang['type:wiki page'] = '이 위키 내부 페이지';
+$lang['type:interwiki'] = '인터위키 링크';
+$lang['type:email'] = '이메일 주소';
+$lang['type:external'] = '외부페이지 링크';
+$lang['type:other'] = '기타';
+$lang['type:automatic title'] = '자동 제목';
+$lang['type:custom title'] = '제목 설정';
+$lang['type:image'] = '이미지로 제목 사용';
+
+$lang['placeholder:link name'] = '(automatic)';
+
+$lang['legend:media'] = '미디어 혹은 파일';
+$lang['label:image_properties'] = '이미지 속성';
+$lang['legend:size'] = '크기';
+$lang['legend:alignment'] = '정렬';
+$lang['legend:linking'] = '링크';
+$lang['legend:caching'] = '캐쉬';
+$lang['media target'] = '이미지 혹은 문서파일';
+$lang['label:caption'] = '캡션 / 링크 문구';
+$lang['label:width'] = '폭 (px)';
+$lang['label:height'] = '높이 (px)';
+$lang['label:default alignment'] = '기본';
+$lang['label:float left'] = '왼쪽';
+$lang['label:center alignment'] = '중간';
+$lang['label:float right'] = '오른쪽';
+$lang['label:default linking'] = '기본';
+$lang['label:direct linking'] = '기본';
+$lang['label:nolink'] = '링크 없슴';
+$lang['label:linkonly'] = '링크만';
+$lang['label:default caching'] = '기본';
+$lang['label:recache'] = '재캐쉬';
+$lang['label:nocache'] = '캐쉬안함';
+
+$lang['cancel'] = '취소';
+
+$lang['js']['cancel'] = $lang['cancel'];
+
+$lang['js']['linkConfig'] = '링크 설정';
+$lang['js']['mediaConfig'] = '미디어 설정';
+$lang['js']['rssConfig'] = 'RSS피드 설정';
+$lang['js']['footnoteViewTitle'] = '변경내용 설명';
+$lang['js']['placeholder:page'] = 'namespace:page';
+
+$lang['js']['label:blockquote'] = '글따옴표';
+$lang['js']['label:bulletList'] = '점목록';
+$lang['js']['label:code'] = '코드블록';
+$lang['js']['label:footnote'] = '각주';
+$lang['js']['label:headings'] = '제목';
+$lang['js']['label:heading'] = '제목 %s';
+$lang['js']['label:media'] = '이미지나 파일 삽입';
+$lang['js']['label:liftLI'] = '목록 내어쓰기';
+$lang['js']['label:link'] = '링크넣기';
+$lang['js']['label:orderedList'] = '숫자목록';
+$lang['js']['label:paragraph'] = '단락';
+$lang['js']['label:pluginBlock'] = '플러긴 블록';
+$lang['js']['label:rss'] = '새 RSS 피드 추가';
+$lang['js']['label:smileys'] = '스마일리';
+$lang['js']['label:settings'] = '페이지설정';
+$lang['js']['label:nocache'] = '캐쉬 끄기';
+$lang['js']['label:notoc'] = '목차 숨기기';
+$lang['js']['label:table'] = '새 2x2 표 삽입';
+$lang['js']['label:table-ops'] = '표 수정';
+$lang['js']['label:table-add-row-before'] = '위에 줄 삽입';
+$lang['js']['label:table-add-row-after'] = '아래에 줄 삽입';
+$lang['js']['label:table-row-delete'] = '줄 삭제';
+$lang['js']['label:table-add-column-before'] = '앞에 열 삽입';
+$lang['js']['label:table-add-column-after'] = '뒤에 열 삽입';
+$lang['js']['label:table-column-delete'] = '열 삭제';
+$lang['js']['label:table-cell-header-toggle'] = '표제 상태 전환';
+$lang['js']['label:table-cell-merge'] = '칸 합치기';
+$lang['js']['label:table-cell-split'] = '칸 분리';
+$lang['js']['label:table-cell-align-left'] = '외쪽 정렬';
+$lang['js']['label:table-cell-align-center'] = '가운데 정렬';
+$lang['js']['label:table-cell-align-right'] = '오른족 정렬';
+$lang['js']['label:table-delete'] = '표 삭제';
+$lang['js']['label:sinkListItem'] = '목록 들여쓰기';
+$lang['js']['label:marks'] = '꾸미기';
+$lang['js']['label:strong'] = '굵게';
+$lang['js']['label:em'] = '기울임';
+$lang['js']['label:underline'] = '밑줄';
+$lang['js']['label:superscript'] = '어깨 글자';
+$lang['js']['label:subscript'] = '첨자';
+$lang['js']['label:deleted'] = '취소선';
+$lang['js']['label:monospaced'] = '고정폭';
+$lang['js']['label:undo'] = '실행취소';
+$lang['js']['label:redo'] = '재실행';
+
+$lang['js']['label:rss url'] = 'RSS 피드 주소';
+$lang['js']['label:rss nOfItems'] = '보여줄 품목의 수';
+$lang['js']['label:rss reverse'] = '마지막 것을 처음에 보여줌';
+$lang['js']['label:rss author'] = '저자 이름 보이기';
+$lang['js']['label:rss date'] = '날짜 보이기';
+$lang['js']['label:rss details'] = '설명 보이기';
+$lang['js']['label:rss refresh'] = '새로고침';
+
+$lang['js']['code_block_hint'] = '💡 나가려면 CTRL+Enter를 누르세요';
